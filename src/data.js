8cdeef46d1adc420a5553b47bff5bcb7f4a50dac
Lynn <lynn.park@optum.com>
2019-08-08 15:41:45 -0400

    some basic stuffs

---
+++ src/data.js
@@ -0,0 +1,22 @@
+const data = [
+  { name: "Happy" },
+  { name: "Dopey" },
+  { name: "Grumpy" },
+  { name: "Sneezy" },
+  { name: "Lynn" },
+  { name: "Bashful" },
+  { name: "Sleepy" },
+  { name: "Doc" },
+  { name: "Bashful" },
+  { name: "Bob" }
+];
+
+// mimics an api call by returning data after a random delay
+function getEmployees() {
+  // get a random timeout value b/w 0.1s and 2s
+  //const timeout = Math.floor(Math.random() * (2000 - 100)) + 100;
+  //setTimeout(() => data, timeout);
+  return data;
+}
+
+export { getEmployees };
