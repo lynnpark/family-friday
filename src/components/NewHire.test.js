1221c2999d9af9c58aff75e59185b90e8f89ec1d
Lynn <lynn.park@optum.com>
2019-08-12 10:47:09 -0400

    fixed oopsie on new hire test

--- src/components/NewHire.test.js
+++ src/components/NewHire.test.js
@@ -2,25 +2,6 @@ import React from "react";
 import { render } from "@testing-library/react";
 import NewHire from "./NewHire";
 
-const group = [
-  {
-    id: 4,
-    name: "Sneezy"
-  },
-  {
-    id: 1,
-    name: "Happy"
-  },
-  {
-    id: 10,
-    name: "Jeff"
-  },
-  {
-    id: 7,
-    name: "Doc"
-  }
-];
-
 describe("NewHire Component", () => {
   test("Matches snapshot", () => {
     const component = render(<NewHire addEmployee={() => {}} />);
