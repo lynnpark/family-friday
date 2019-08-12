044359bea8b0a6d0c9cd8b6c277e465839444224
Lynn <lynn.park@optum.com>
2019-08-12 13:58:02 -0400

    comments

--- src/components/FamilyFriday.js
+++ src/components/FamilyFriday.js
@@ -38,15 +38,24 @@ export default function FamilyFriday() {
   );
 }
 
+/**
+ * Takes an array of employee objects and returns an array of groups.
+ * A group has 3-5 employee objects
+ *
+ * @param {Object[]} employees
+ */
 export function getGroups(employees) {
   if (!employees) return null;
   const employeesArr = shuffle(employees.slice(0));
   const groups = [];
 
+  // Loops through and places the first 4 employee objects to a group.
   while (employeesArr.length) {
     let group = [];
     if (employeesArr.length === 5) group = employeesArr.splice(0, 5);
+    //if there is 5 left, put all 5 in a group
     else if (employeesArr.length === 2)
+      //if there is 2 left, take one from the previous group to create a group of 3
       group = [...employeesArr.splice(0, 2), groups[groups.length - 1].pop()];
     else group = employeesArr.splice(0, 4);
     groups.push(group);
@@ -55,7 +64,11 @@ export function getGroups(employees) {
   return groups;
 }
 
-// shuffles employees using Fisher-Yates algorithm
+/**
+ * Shuffles an array using the Fisher-Yates algorithm
+ *
+ * @param {Array} data
+ */
 function shuffle(data) {
   let i = data.length;
   while (i--) {
