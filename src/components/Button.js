59e7600379ca4c5ca7caa671b644eb2705da76a2
Tom Collier <collier@apartmentlist.com>
2020-01-27 18:22:33 -0800

    Move css const to outside component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,38 +3,6 @@
 import { css, jsx } from "@emotion/core";
 
 export default function Button(props) {
-  const commonCss = `
-    border: transparent;
-    border-radius: 6px;
-    text-transform: capitalize;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    background: linear-gradient(to bottom,#8434d6 0%,#8434d6 100%);
-    color: #fff;
-    cursor: pointer;
-    margin: 8px;
-    &:hover {
-      background: linear-gradient(to bottom,#9e5edf 0%,#9e5edf 100%);
-    }
-  `
-
-  const cssOpts = {
-    medium: `
-      font-size: 18px;
-      font-weight: 500;
-      height: 48px;
-      padding: 10px 25px;
-    `,
-    large: `
-      font-size: 22px;
-      font-weight: 800;
-      height: 54px;
-      padding: 12px 27px;
-    `
-  }
-
   return (
     <button
       aria-label={props.label}
@@ -51,4 +19,36 @@ export default function Button(props) {
 
 Button.defaultProps = {
   size: "medium"
+}
+
+const commonCss = `
+border: transparent;
+border-radius: 6px;
+text-transform: capitalize;
+-webkit-user-select: none;
+-moz-user-select: none;
+-ms-user-select: none;
+user-select: none;
+background: linear-gradient(to bottom,#8434d6 0%,#8434d6 100%);
+color: #fff;
+cursor: pointer;
+margin: 8px;
+&:hover {
+  background: linear-gradient(to bottom,#9e5edf 0%,#9e5edf 100%);
+}
+`
+
+const cssOpts = {
+medium: `
+  font-size: 18px;
+  font-weight: 500;
+  height: 48px;
+  padding: 10px 25px;
+`,
+large: `
+  font-size: 22px;
+  font-weight: 800;
+  height: 54px;
+  padding: 12px 27px;
+`
 }
